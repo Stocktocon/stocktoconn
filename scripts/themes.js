9f672061f844aa39a149896d2f2ab2bd35ecb9e1
Huang, Connor <chh056@ucsd.edu>
2025-03-02 17:52:31 -0800

    themes applied to home and contact page

--- scripts/themes.js
+++ scripts/themes.js
@@ -1,18 +1,21 @@
 /*
     greatly pulled from the hw4 discussion stuff
 */
-
+//try catch on the footer is because it causes an error and breaks the slider if there is no footer in the page (should be fixed when everything has a footer by the SSG)
 function setTheme(theme) {
     const root = document.documentElement;
     if (theme === 'dark'){
         root.style.setProperty('background-color', 'var(--background-color-dark, black)');
         root.style.setProperty('color', 'var(--text-color-dark, white)');
-        document.querySelector('footer').style.setProperty('color', 'var(--faded-text-dark, #bcbcbc)');
+        try {document.querySelector('footer').style.setProperty('color', 'var(--faded-text-dark, #bcbcbc)');
+        }catch {}
     }
     else{
+        
         root.style.setProperty('background', 'var(--background-color, white)');
         root.style.setProperty('color', 'var(--text-color, black)');
-        document.querySelector('footer').style.setProperty('color', 'var(--faded-text, #2c2c2c)');
+        try{document.querySelector('footer').style.setProperty('color', 'var(--faded-text, #2c2c2c)');
+        } catch{}
     }
 }
 function toggleTheme()
@@ -20,14 +23,13 @@ function toggleTheme()
     const currTheme = localStorage.getItem('theme') || 'light';
     const newTheme = currTheme === 'light'? 'dark': 'light';
     localStorage.setItem('theme', newTheme);
-    setTheme(newTheme);
 }
 
 function setSavedTheme() {
     const savedTheme = localStorage.getItem('theme');
     if(savedTheme) {
         setTheme(savedTheme);
-        document.getElementById('themeToggle').checked = (savedTheme === 'dark');
+        document.getElementById('themeToggle').checked = (savedTheme == 'dark');
     }
 }
 
