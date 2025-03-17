d8bbf662a3e42a7a7e6a59500d3c26ef8a258afd
Huang, Connor <chh056@ucsd.edu>
2025-03-16 17:10:31 -0700

    added functionality of the hide nav bar.

--- scripts/hide.js
+++ scripts/hide.js
@@ -1,1 +1,14 @@
 // do stuff here similar to the light mode dark mode but instead modify the DOM and add stuff into the nav header when necessary. 
+
+function doHide() {
+    root = document.documentElement;
+    hidden = localStorage.getItem('weaf');
+    if (hidden === 'true'){
+        root.style.setProperty('--hide', 'none');
+    }
+    else{
+        root.style.setProperty('--hide', 'block');
+    }
+}
+
+doHide();
